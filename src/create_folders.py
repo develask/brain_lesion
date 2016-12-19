134289b4859cd9b5331f3acc16b1b40dac73088c
alopez247 <alopez247@hotmail.com>
2016-12-19 13:15:56 +0100

    proyect initalized

---
+++ src/create_folders.py
@@ -0,0 +1,47 @@
+import os
+import errno    
+import os
+
+
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+mkdir_p("../data")
+
+wanted_dirs = ['FA', 'flair', 'L1', 'L2', 'L3', 
+			    'MD', 'MO', 'S0', 'V1', 'V2', 'anatomica']
+
+for d in wanted_dirs:
+	mkdir_p("../data/"+d)
+
+# for elem in os.walk("."):
+# 	direcs= elem
+# 	break
+
+# dirs = direcs[1]
+
+for d in wanted_dirs:
+		mkdir_p("../data/"+d+"/normalized")
+		mkdir_p("../data/"+d+"/div_2D"+"/x")
+		mkdir_p("../data/"+d+"/div_2D"+"/y")
+		mkdir_p("../data/"+d+"/div_2D"+"/z")
+		mkdir_p("../data/"+d+"/slices")
+
+mkdir_p("../data/mask")
+mkdir_p("../data/raw")
+
+
+	#if direc != datos or 
+	#os.mkdirs(direc)
+
+
+
+
---
+++ src/normalize.py
@@ -0,0 +1,95 @@
+import os
+import numpy as np
+import nibabel as nib
+import sys
+
+import re
+
+
+#img = nib.load("datos/004/tka004_lesion_mask.nii.gz")
+#data = img.get_data()
+
+#img = nib.load("datos/003/tka003_dwi_MD.nii.gz")
+#data2 = img.get_data()
+
+
+#print("V1")
+#print(len(data),len(data[0]),len(data[0][0]))
+
+#print("MD")
+#print(len(data2),len(data2[0]),len(data2[0][0]))
+
+#quit()
+
+#data = data[126]
+
+#img = nib.Nifti1Image(data, np.eye(4))
+#nib.save(img,"datos/003/tka003_dwi_L3_duplicado_2D.nii.gz")
+
+#,quit()
+
+def std_data(data,valor_min=0,valor_max=1):
+	max_pre = np.max(data) # valor maximo en data
+	min_pre = np.min(data)
+
+	len_1 = len(data)
+	len_2 = len(data[0])
+	len_3 = len(data[0][0])
+
+	for i in range(len_1):
+		for j in range(len_2):
+			for k in range(len_3):
+				data[i][j][k] = (data[i][j][k] - (min_pre - valor_min)) / (max_pre-min_pre) * (valor_max-valor_min)
+
+ind=0
+def getImgType(filename):
+	#tipo = ""
+	#for i in range(len(filename)):
+	#	if filename[i]=="_":
+	#		ind=i
+	#i = ind
+	#while filename[i] != ".":
+	#	 tipo += filename[i]
+	#	i += 1
+	#return(tipo)
+
+	#mask = '_[a-zA-Z]+\.nii.gz'
+	#result = re.search(mask).match(filename)
+	#mask = 'nii.gz'
+	#p = re.compile(mask);
+	#result = p.match(filename)
+	result = re.search(".*\_([a-zA-Z0-9]+)\.nii\.gz",filename).group(1)
+	print(result)
+	return (result)
+
+#std_data(data)
+
+#print(np.min(data))
+#print(np.max(data))	
+
+dir_name = sys.argv[1]
+for archivo in os.walk("../data/raw/"+dir_name):
+	direcs = archivo
+	break
+images = direcs[2]
+
+for image in images:
+	print("IMAGE " + image)
+	try:
+		print("i'm trying...")
+		img = nib.load("../data/raw/" + dir_name + "/" + image)
+		print("try finished")
+
+		data = img.get_data()
+		std_data(data)
+		img = nib.Nifti1Image(data, np.eye(4))
+		tipo = getImgType(image)
+		#nib.save(img,"../data/tipo/"+image[0:-8]+"_norm.nii.gz")
+		print("../data/" + tipo + "/" + image[0:-7] + "_norm.nii.gz")
+		nib.save(img, "../data/" + tipo + "/normalized/" + image[0:-7] + "_norm.nii.gz")
+	except:
+		print("EOFError in IMAGE:",image)
+		print("passing...")
+		continue
+
+
