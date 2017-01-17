0a468dd7558acd6d63985b5750a7d858f3d2409e
develask <develascomikel@gmail.com>
2017-01-17 10:49:33 +0100

    Change preprocess to numpy funtions and predict

---
+++ src/brain.py
@@ -0,0 +1,120 @@
+import numpy as np
+import nibabel as nib
+import os
+
+class Brain():
+	def __init__(self, brain):
+		self.name = brain
+		self.mask = nib.load("../data/mask/normalized/" + brain + "_lesion_mask_norm.nii.gz").get_data()>0
+		self.lenx = len(self.mask)
+		self.leny = len(self.mask[0])
+		self.lenz = len(self.mask[0][0])
+		self.types = {}
+
+	def load(self, newtype):
+		if not newtype in self.types:
+			for f in os.walk("../data/"+newtype+"/normalized/"):
+				direcs = f
+				break
+			images = direcs[2]
+			for i in range(len(images)):
+				name = images[i]
+				try:
+					if name.index(self.name)>-1:
+						self.types[newtype] = nib.load("../data/"+newtype+"/normalized/" + name).get_data()
+				except:
+					pass
+
+	def createSlices(self, step):
+		standar = nib.load("../data/standars/MNI152_T1_1mm_first_brain_mask.nii.gz").get_data()
+		indexes = np.indices((int(self.lenx/step), int(self.leny/step), int(self.lenz/step)))
+		tmp = self.mask[::step,::step,::step]
+		if (tmp.shape[0] != indexes.shape[1]):
+			tmp = tmp[:-1,:-1,:-1]
+		length = tmp.shape[0]*tmp.shape[1]*tmp.shape[2]
+		self.result = np.concatenate((
+			indexes[0].reshape(1,length).T*step,
+			indexes[1].reshape(1,length).T*step,
+			indexes[2].reshape(1,length).T*step,
+			tmp.reshape(1,length).T
+		), axis=1)
+		self.result = np.array([a for a in self.result if standar[a[0],a[1],a[2]]>0])
+
+	def balance(self, bal):
+		pos = self.result[self.result[:,3]==1]
+		neg = self.result[self.result[:,3]==0]
+		neg = neg[:pos.shape[0]*bal]
+		pos = np.concatenate((pos,neg), axis=0)
+		np.random.shuffle(pos)
+		self.result = pos
+
+	def split(self, portion):
+		split_num = int(portion * self.result.shape[0])
+		self.train = self.result[:split_num]
+		self.test = self.result[split_num:]
+
+	def getData(self, img_types, sample_type, dim):
+		result_train = None
+		result_test = None
+		for img_type in img_types:
+			self.load(img_type)
+			margin = np.full((self.mask.shape[0]+dim-1, self.mask.shape[1]+dim-1, self.mask.shape[2]+dim-1), self.mask[0,0,0])
+			mar = int((dim-1)/2)
+			margin[mar:-mar, mar:-mar, mar:-mar] = self.types[img_type][:,:,:]
+			if sample_type == "2dx":
+				train_X = np.empty((self.train.shape[0], dim, dim))
+				i = 0
+				for x in self.train:
+					train_X[i,:,:] = margin[x[0]+mar-1,x[1]:x[1]+dim,x[2]:x[2]+dim]
+					i+=1
+				test_X = np.empty((self.test.shape[0], dim, dim))
+				i = 0
+				for x in self.test:
+					test[i,:,:] = margin[x[0]+mar-1,x[1]:x[1]+dim,x[2]:x[2]+dim]
+					i+=1
+			if sample_type == "2dy":
+				train_X = np.empty((self.train.shape[0], dim, dim))
+				i = 0
+				for x in self.train:
+					train_X[i,:,:] = margin[x[0]:x[0]+dim,x[1]+mar-1,x[2]:x[2]+dim]
+					i+=1
+				test_X = np.empty((self.test.shape[0], dim, dim))
+				i = 0
+				for x in self.test:
+					test[i,:,:] = margin[x[1]:x[1]+dim,x[0]+mar-1,x[2]:x[2]+dim]
+					i+=1
+			if sample_type == "2dz":
+				train_X = np.empty((self.train.shape[0], dim, dim))
+				i = 0
+				for x in self.train:
+					train_X[i,:,:] = margin[x[0]:x[0]+dim,x[1]:x[1]+dim,x[2]+mar-1]
+					i+=1
+				est_X = np.empty((self.test.shape[0], dim, dim))
+				i = 0
+				for x in self.test:
+					test[i,:,:] = margin[x[1]:x[1]+dim,x[2]:x[2]+dim,x[0]+mar-1]
+					i+=1
+			if sample_type == "3d":
+				train_X = np.empty((self.train.shape[0], dim, dim, dim))
+				i = 0
+				for x in self.train:
+					train_X[i,:,:,:] = margin[x[0]:x[0]+dim,x[1]:x[1]+dim,x[2]:x[2]+dim]
+					i+=1
+				test_X = np.empty((self.test.shape[0], dim, dim, dim))
+				i = 0
+				for x in self.test:
+					test_X[i,:,:,:] = margin[x[0]:x[0]+dim,x[1]:x[1]+dim,x[2]:x[2]+dim]
+					i+=1
+			if type(result_train) == type(None):
+				if sample_type == "3d":
+					result_train = train_X[:,:,:,:, np.newaxis]
+					result_test = test_X[:,:,:,:, np.newaxis]
+				else:
+					result_train = train_X[:,:,:, np.newaxis]
+					result_train = test_X[:,:,:, np.newaxis]
+			else:
+				result_train=np.insert(result_train, result_train.shape[-1], train_X, axis=len(result_train.shape)-1)
+				result_test=np.insert(result_test, result_test.shape[-1], test_X, axis=len(result_test.shape)-1)
+		return([(result_train, self.train[:,3]), (result_test, self.test[:,3])])
+
+
--- src/predict.py
+++ src/predict.py
@@ -1,64 +1,45 @@
 import os
-import image_functions as imf
 from keras.models import load_model
-import sample_cretation as sc
 import nibabel as nib
 import numpy as np
 import math
+import brain as br
 
 
 modelo_file = "../models/model_0.mdl"
+result_path = "../results/new_image2.nii.gz"
 
 model = load_model(modelo_file)
 
+brain_id = "tka004"
+
 inp_dim = 19
-step = 1
 img_types = ["flair"]
 sample_type = "2dx"
 
-cerebro = "tka004"
-print("Calculando parte del cerebro")
-ex = sc.Examples()
-ex.initilize(cerebro)
-ex.get_examples(step = step,output_type="classes")
-ex.getData([], img_types, "2dx", inp_dim, crbs = cerebro)
+brain = br.Brain(brain_id)
+brain.createSlices(step=1)
+brain.split(1)
 
-brainim = nib.load("../data/standars/MNI152_T1_1mm_first_brain_mask.nii.gz")
-brain = imf.OurImage(brainim.get_data(), "brain")
-x = ex.getData([ex[0][0]], img_types, "2dx", inp_dim, crbs = cerebro)
-x = np.asarray(x)
-x.reshape(x.shape[0], inp_dim, inp_dim,len(img_types))
-print("Emaitza")
-total = len(ex.pairs)
-start = 0
-step = math.floor(total/100)
-stop = step-1
-print(stop*100/float(total),"%     ",stop," - ",total, end="\r",flush=True)
-while True:
-	l=[]
-	for index in range(start, stop):
-		l.append(ex[index][0])
-	x = ex.getData(l, img_types, "2dx", inp_dim, crbs = cerebro)
-	x = np.asarray(x)
-	x = x.reshape(x.shape[0], inp_dim, inp_dim,len(img_types))
-	x = model.predict(x)
-	i=0
-	for a in x:
-		y_pred = a[1] >= 0.5
-		brain.data[l[i].x][l[i].y][l[i].z] = float(1 if y_pred else 0)
-		i+=1
-	print(stop*100/float(total),"%     ",stop," - ",total, end="\r",flush=True)
-	start+=step
-	stop+=step
-	if (stop>total):
-		stop=total
-	if(start>total):
-		break
+total = brain.getData(img_types, sample_type, inp_dim)
+x = total[0][0]
+y_real = total[0][1]
 
-print("end                               ")
+y_pred = model.predict(x)
+y_pred = y_pred[:,1] >= 0.5
 
-new_image = nib.Nifti1Image(brain.data,  np.eye(4))
-nib.save(new_image, 'new_image2.nii.gz')
+brain.train = np.concatenate((
+	brain.train,
+	y_pred.T
+), axis=1)
+
+new_im = np.zeros(brain.mask)
+for a in brain.train:
+	new_im[a[0],a[1],a[2]] = a[4]
+	
+
+new_image = nib.Nifti1Image(new_im,  np.eye(4))
+nib.save(new_image, result_path)
 
 
 
