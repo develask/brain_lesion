47b9335adcb9e61fae3ee9bcda19a61b53ba432f
develask <develascomikel@gmail.com>
2017-01-24 11:50:17 +0100

    thresshold

---
+++ src/thresshold.py
@@ -0,0 +1,13 @@
+import nibabel as nib
+import numpy as np
+import sys
+
+
+image = sys.argv[1]
+image_r = sys.argv[2]
+t = sys.argv[3]
+
+i = nib.load(image).get_data()
+i = i >= float(t)
+new_image = nib.Nifti1Image(i,  np.eye(4))
+nib.save(new_image, t)
