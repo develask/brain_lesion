8cf0b9b00bd9468bc31ad1ae04fecd37f4c87448
develask <develascomikel@gmail.com>
2017-02-15 09:46:32 +0100

    Model Function created

--- src/ModelFunctions.py
+++ src/ModelFunctions.py
@@ -24,31 +24,32 @@ cv_history = []
 
 
 class Model():
-	def __init__(self, model, dataManager):
+	def __init__(self, model_name, model, getDataFunc):
+		self.model_name = model_name
 		self.model = model
-		self.dataManager = dataManager
+		self.getDataFunc = getDataFunc
 		self.brains = ["tka002","tka003","tka004","tka005","tka006","tka007","tka009","tka010","tka011","tka012","tka013","tka015","tka016","tka017","tka018","tka019","tka020","tka021"]
 
 
-	def leaveOneOut(self, nb_epoch = 250, batch_size = 128):
-
-		init_ler = 0.05
-		final_ler = 0.005
+	def leaveOneOut(self, nb_epoch = 250, batch_size = 128, init_ler = 0.05, final_ler = 0.005):
 		dec = (final_ler/init_ler)**(1/nb_epoch)
 
+		traindata = DataManager(self.getDataFunc)
 
 		for i in range(len(self.brains)):
 			test = [self.brains[i]]
 			train = self.brains[0:i] + self.brains[i+1:len(self.brains)]
 
-			self.dataManager.setTrain(train)
+			traindata.setTrain(train)
+
+			cv_history = []
 
 			ler = init_ler
 			for j in range(nb_epoch):
 				print("Starting epoch:", j+1, "/", nb_epoch)
 				print("Genrating new training data")
 				
-				d = self.dataManager.getData()
+				d = traindata.getData()
 
 				sgd = SGD(lr=ler,decay=0,momentum=0.0,nesterov = False)
 				self.model.compile(loss='binary_crossentropy',
@@ -59,25 +60,21 @@ class Model():
 				cv_history.append(tr_h.history["loss"][0])
 				ler *= dec
 
-			self.model.save("../models/model_" + model_name +"_"+ str(i) + ".mdl")
+			self.model.save("../models/model_" + self.model_name +"_for_"+ test[0] + ".mdl")
+			with open("hist_"+ self.model_name +"_for_"+ test[0] +".json","w") as tf:
+				tf.write(json.dumps(cv_history))
 
 class DataManager():
-	def __init__(self):
+	def __init__(self, datafunc):
 		self.tr = imm.ImageManager()
+		self.datafunc = datafunc
 
 	def setTrain(self, brains):
 		self.tr.reset()
 		self.tr.init(brains)
 
-	def getData(self):	
-		self.tr.createSlices(step=step)
-		self.tr.balance(bal_train)
-		self.tr.split(1)
-		X_train_x = self.tr.getData(img_types, "2dx", inp_dim_2d)[0]
-		y_train = X_train_x[1]
-		X_train_x = X_train_x[0]
-		X_train_y = self.tr.getData(img_types, "2dy", inp_dim_2d)[0][0]
-		X_train_z = self.tr.getData(img_types, "2dz", inp_dim_2d)[0][0]
-		X_train_3d = self.tr.getData(img_types, "3d", inp_dim_3d)[0][0]
+	def getData(self):
+		return self.datafunc(self.tr)
 
-		return(([X_train_x,X_train_y, X_train_z, X_train_3d], y_train))
+
+		
